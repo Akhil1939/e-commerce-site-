2d0b4d74e6d1f09d7707ca37128b0aceb57c0afc
akhil1939 <akhilkukadiya464@gmail.com>
2022-08-03 21:44:50 +0530

    commit-1

--- back/app.js
+++ back/app.js
@@ -1,19 +1,22 @@
 const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+require('dotenv').config();
 
 
-mongoose.connect("mongodb://localhost:27017/tshirt",{
+mongoose.connect(process.env.DATABASE,{
     useNewUrlParser: true ,
     useUnifiedTopology:true,
     useCreateIndex:true
 }).then(()=>{
     console.log("DB connected....")
 }
-);
+).catch(()=>{
+    console.log("oopps db not connected..")
+})
 
-const port = 8000;
+const port = process.env.PORT || 8000;
 
 app.listen(port, ()=>{
     console.log(`app is running at ${port}...`);
-})
+}) 
